9b34c34c83026d89d5aa1570f5f370ce606d3ecd
cdiaz2024 <cgd6055@gmail.com>
2024-06-06 22:32:36 -0500

    Slideshow Effect

--- src/DispTranslate.jsx
+++ src/DispTranslate.jsx
@@ -1,16 +1,54 @@
-import React,{useState,useEffect} from 'react';
-import mainPicture from './PAMainPage.png' 
-function DispTranslate(){
-    return(
-        <div>
-        <section>
+import React, { useState } from 'react';
+import mainPicture from './PAMainPage.png';
+import picture2 from './PAPicture2.png';
+import picture3 from './PAPicture3.png';
+import picture4 from './PAPicture4.png';
+import './DispTranslate.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-        </section>
-        <footer>
-            <p><a href='https://www.perfect-accent.com' target='_Blank'>Live Demo</ a> | <a href='https://www.github.com/cdiaz2024/translations' target='_Blank'>GitHub Repo</a></p>
-        </footer>
+function DispTranslate() {
+    const pictures = [mainPicture, picture2, picture3, picture4];
+    const [dispPicture, setDispPicture] = useState(mainPicture);
+
+    const handleLeftClick = () => {
+        const currentIndex = pictures.indexOf(dispPicture);
+        let newIndex = currentIndex-1;
+        if(newIndex<0){
+            newIndex=pictures.length-1;
+        }
+        setDispPicture(pictures[newIndex]);
+    }
+
+    const handleRightClick = () => {
+        const currentIndex = pictures.indexOf(dispPicture);
+        let newIndex = currentIndex+1;
+        console.log(newIndex)
+        console.log(pictures.length)
+        if(newIndex===pictures.length){
+            newIndex=0;
+        }
+        setDispPicture(pictures[newIndex]);
+    }
+
+    return (
+        <div className='translateBox'>
+            <div className='buttonContainer'>
+                <button className='LeftButton' onClick={handleLeftClick}>
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+                <section>
+                    <img src={dispPicture} alt='Display' />
+                </section>
+                <button className='RightButton' onClick={handleRightClick}>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+            </div>
+            <footer>
+                <p><a href='https://www.perfect-accent.com' target='_Blank'>Live Demo</a> | <a href='https://www.github.com/cdiaz2024/translations' target='_Blank'>GitHub Repo</a></p>
+            </footer>
         </div>
-    )
+    );
 }
 
 export default DispTranslate;
--- src/DispWorkout.jsx
+++ src/DispWorkout.jsx
@@ -1,8 +1,62 @@
 import React,{useState,useEffect} from 'react';
+import workout1 from './Workout1.png';
+import workout2 from './Workout2.png';
+import workout3 from './Workout3.png';
+import workout4 from './Workout4.png';
+import workout5 from './Workout5.png';
+import workout6 from './Workout6.png';
+import workout7 from './Workout7.png';
+import workout8 from './Workout8.png';
+import './DispWorkout.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function DispWorkout(){
+    const pictures = [workout1,workout2,workout3,workout4,workout5,workout6,workout7,workout8];
+    const [dispPicture, setDispPicture] = useState(workout1);
+
+    const handleLeftClick = () => {
+        const currentIndex = pictures.indexOf(dispPicture);
+        let newIndex = currentIndex-1;
+        if(newIndex<0){
+            newIndex=pictures.length-1;
+        }
+        setDispPicture(pictures[newIndex]);
+    }
+
+    const handleRightClick = () => {
+        const currentIndex = pictures.indexOf(dispPicture);
+        let newIndex = currentIndex+1;
+        console.log(newIndex)
+        console.log(pictures.length)
+        if(newIndex===pictures.length){
+            newIndex=0;
+        }
+        setDispPicture(pictures[newIndex]);
+    }
+
+
     return(
-        'Workout Tracker Component'
+        <div className='WorkoutBox'>
+            <div className='buttonContainer'>
+                <button className='LeftButton' onClick={handleLeftClick}>
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+                <section>
+                    <img src={dispPicture} alt='Display'/>
+                </section>
+                <button className='RightButton' onClick={handleRightClick}>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+            </div>
+            <footer>
+                <p>
+                    <a href='https://www.mybulkbuddy.com' target='_blank'>Live Demo</a>
+                    |
+                    <a href='https://www.github.com/csnse/workouttracker' target='_blank'>GitHub Repo</a>
+                </p>
+            </footer>
+        </div>
     )
 }
 
